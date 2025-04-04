b5af7fb9914bd3352f706db2dd1ecef8832ad039
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-04-04 21:39:00 +0530

    Delete jsp-servlet-examples directory

--- jsp-servlet-examples/hello-world/src/main/java/com/vaishnavi/practice/servlet/service/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.vaishnavi.practice.servlet.service;
-
-public class HelloService {
-    public String greet(String name) {
-        return "hello " + (name.isBlank() ? "world!!" : name);
-    }
-}
-
